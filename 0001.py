083cf1c1eacae3b555aab2062874f45ea1b03941
Ismail Arai <ismail0311@gmail.com>
2015-07-14 00:26:43 +0900

    Fix indent

--- 0001.py
+++ 0001.py
@@ -2,6 +2,6 @@
 
 sum = 0
 for num in range(1, 1000):
-	if num % 3 == 0 or num % 5 == 0:
-		sum += num
+    if num % 3 == 0 or num % 5 == 0:
+    sum += num
 print('Sum of FizzBuzz from 1 to 999: ' + str(sum))
--- 0003.py
+++ 0003.py
@@ -5,10 +5,10 @@ prime_list = []
 prime = 2
 
 while prime <= x:
-	while x % prime is 0:
-		prime_list.append(prime)
-		x //= prime
-	prime += 1
+    while x % prime is 0:
+        prime_list.append(prime)
+        x //= prime
+    prime += 1
 
 #print(prime_list)
 print('The largest prime factor of the number 600851475143: ' + str(prime_list[-1]))
