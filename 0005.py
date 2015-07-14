e194609560b56a3c8e36e89d7bb8798ad62738da
Ismail Arai <ismail0311@gmail.com>
2015-07-14 17:21:02 +0900

    add problem 5

---
+++ 0005.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# __author__ = 'ismail'
+
+import math
+
+p = [1, 2, 3, 5, 7, 11, 13, 17, 19]
+a = [1] * len(p)
+k = 20
+N = 1
+i = 1
+for i in range(1, len(p)):
+    if p[i] < math.sqrt(k):
+        a[i] = int(math.log(k, 2) / math.log(p[i], 2))
+    N *= p[i] ** a[i]
+
+print('The smallest positive number that is evenly divisible by all of the numbers from 1 to 20: ' + str(N))
